b9d69e56e505b923f01bbb90a44be7bd15d50dc0
Luciano Pueyo <l.pueyo@bue.edu.ar>
2023-04-04 22:39:17 -0300

    Clase 11 - urls con varios parametros - expresiones regulares

--- Django/codo_codo_2023/aula_virtual/urls.py
+++ Django/codo_codo_2023/aula_virtual/urls.py
@@ -1,9 +1,15 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
     path('', views.landing_page, name='landing_page'),
-    path('saludar/<str:nombre_usuario>', views.saludar_usuario, name='saludar_usuario'),
-    path('alta_alumno', views.alta_alumno, name='alta_alumno'),
-    path('baja_alumno', views.baja_alumno, name='baja_alumno')
+    path('saludar/<str:nombre_usuario>/', views.saludar_usuario, name='saludar_usuario'),
+    path('alta_alumno/', views.alta_alumno, name='alta_alumno'),
+    path('baja_alumno/', views.baja_alumno, name='baja_alumno'),
+
+    path('alumnos/2023/', views.listar_alumnos_2023, name="listar_alumnos_2023"),
+    re_path(r'^alumnos/(?P<year>[0-9]{4})/$', views.listar_alumnos_by_year, name="listar_alumnos_by_year"),
+    re_path(r'^alumnos/(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/$', views.listar_alumnos_by_year_month, name="listar_alumnos_by_year_month"),
+
+    path('docentes/<int:year>/', views.docentes_by_year, {'curso': 'Django'}, name="docentes_by_year"),
 ]
--- Django/codo_codo_2023/aula_virtual/views.py
+++ Django/codo_codo_2023/aula_virtual/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
+from django.urls import reverse
 
 def landing_page(request):
-    return HttpResponse("<h1>Bienvenid@</h1> al Aula Virtual 2.0")
+    print(reverse('alta_alumno'))
+    print(request.method)
+
+    return HttpResponse("<h1>Bienvenid@</h1> al Aula Virtual 2.0", status=208)
 
 def saludar_usuario(request, nombre_usuario):
     return HttpResponse(f"Hola <b>{nombre_usuario}</b>, Bienvenid@ al Aula Virtual 2.0")
@@ -12,3 +16,44 @@ def alta_alumno(request):
 
 def baja_alumno(request):
     return HttpResponse("<h2>Baja de alumnos activos</h2>")
+
+def listar_alumnos_2023(request):
+    return HttpResponse(
+        "<h2>Listado de alumnos 2023</h2>" + 
+        "<p>1 - Carlos Lopez</p>" + 
+        "<p>2 - Maria Del Cerro</p>" + 
+        "<p>3 - Jorge Gomez</p>" 
+    )
+
+def listar_alumnos_by_year(request, year):
+    if int(year) < 2020:
+        return HttpResponseNotFound("<h2>No hay datos previos al año 2020</h2>")
+
+    return HttpResponse(
+        f"<h2>Listado de alumnos {year}</h2>" + 
+        "<p>Josefina Perez (2023)</p>" + 
+        "<p>Raul Diaz (2022)</p>" + 
+        "<p>Micaela Gonzalez(2021)</p>" 
+    )
+    
+def listar_alumnos_by_year_month(request, year, month):
+    if int(year) < 2020:
+        return HttpResponseNotFound("<h2>No hay datos previos al año 2020</h2>")
+
+    if int(month) > 4:
+        return HttpResponseNotFound("<h2>Sólamente hay datos para el primer cuatrimestre del año</h2>")
+    
+    return HttpResponse(
+        f"<h2>Listado de alumnos año:{year} mes:{month}</h2>" + 
+        f"<p>Josefina Perez ({month}/{year})</p>" + 
+        f"<p>Raul Diaz ({month}/{year})</p>" + 
+        f"<p>Micaela Gonzalez({month}/{year})</p>" 
+    )
+
+def docentes_by_year(request, year, curso):
+    print(curso)
+    return HttpResponse(
+        f"<h2>Listado de docentes año: {year} del curso: {curso}</h2>" + 
+        "<p>Docente 1</p>" + 
+        "<p>Docente 2</p>"
+    )
