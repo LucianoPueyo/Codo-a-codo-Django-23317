f0472f6435045707e03cecd93c7ad6c50c032394
Luciano Pueyo <l.pueyo@bue.edu.ar>
2023-05-13 00:20:56 -0300

    Clase 22: Modelos 2 - Relaciones

--- Django/codo_codo_2023/aula_virtual/models.py
+++ Django/codo_codo_2023/aula_virtual/models.py
@@ -4,5 +4,26 @@ class Alumno(models.Model):
     nombre = models.CharField(max_length=128, verbose_name="Nombre")
     apellido = models.CharField(max_length=128, verbose_name="Apellido")
     mail = models.EmailField(max_length=128, verbose_name="Email")
-    fecha_nacimiento = models.DateField(verbose_name="Fecha de nacimiento", default='1900-01-01')
     dni = models.IntegerField(verbose_name="Dni", null=True)
+
+
+class DetalleAlumno(models.Model):
+    fecha_nacimiento = models.DateField(verbose_name="Fecha de nacimiento", default='1900-01-01')
+    fecha_ingreso = models.DateField(verbose_name="Fecha de ingreso al sistema", default='1900-01-01')
+    direccion = models.CharField(max_length=256, verbose_name="Dirección")
+    mail_alternativo = models.EmailField(max_length=128, verbose_name="Email alternativo")
+    titulo = models.BooleanField(verbose_name="Titulo secundario entregado")
+    alumno = models.OneToOneField(Alumno, on_delete=models.CASCADE, primary_key=True) # Uno a uno
+
+
+class Instructor(models.Model):
+    nombre = models.CharField(max_length=128, verbose_name="Nombre")
+    apellido = models.CharField(max_length=128, verbose_name="Apellido")
+    mail = models.EmailField(max_length=128, verbose_name="Email")
+    dni = models.IntegerField(verbose_name="Dni", null=True)
+
+
+class Curso(models.Model):
+    comision = models.IntegerField(verbose_name="Número de comisión")
+    instructor = models.ForeignKey(Instructor, on_delete=models.CASCADE) # Muchos a uno
+    alumnos = models.ManyToManyField(Alumno)
